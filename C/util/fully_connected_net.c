9ff4563caf4e9a445112d77567969bcab29586e4
Bin Tang <13inback@gmail.com>
2017-07-25 09:14:53 +0800

    update

--- C/util/fully_connected_net.c
+++ C/util/fully_connected_net.c
@@ -4,12 +4,32 @@
 #include <math.h>
 #include "layers.h"
 #include "fully_connected_net.h"
+#include <malloc.h>
+#include <stdarg.h>
 
 int fullyConnectedNets(TwoDMatrix* X, int minibatch_size) {
 
 }
 
-int initFCNetwork() {
+int initParameters(TwoDMatrix* X, 
+	TwoDMatrix* correct_labels, 
+	int minibatch_size, 
+	int labels, 
+	float learning_rate, 
+	float reg_strength, 
+	float alpha, 
+	int network_depth, ...) {
+	va_list size_configs;
+	int layer_sizes[network_depth];
+	va_start(size_configs,network_depth);
+	for(int i=0;i<network_depth;i++) {
+		layer_sizes[i] = va_arg(size_configs,int);
+	}
+	va_end(size_configs);
+	// The last layer is the label layer, so you don't have control on the size of it
+	layer_sizes[network_depth-1] = labels;
+}
+
 	/*
 	 How will the size of the Ws and Hs determined?
 	 Like below (X is the input data):
@@ -22,4 +42,6 @@ int initFCNetwork() {
 		former_width = hidden_layer_sizes[i];
 	 }
 	 */
+int train(parameters* networkParams) {
+	
 }
