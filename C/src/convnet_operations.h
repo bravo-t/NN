7db0dfdfc9054e8909b7ead9fbb5ee3b7a15cda4
Bin Tang <13inback@gmail.com>
2017-08-23 20:08:45 +0800

    update

--- C/src/convnet_operations.h
+++ C/src/convnet_operations.h
@@ -1,6 +1,10 @@
 #ifndef __CONVNET_OPTS_HEADER__
 #define __CONVNET_OPTS_HEADER__
 
+int calcOutputSize(int length, int padding, int filter_length, int stride);
+ThreeDMatrix* zeroPadding(ThreeDMatrix* X, int padding_height, int padding_width);
+int convSingleFilter(ThreeDMatrix* X,ThreeDMatrix* F,ThreeDMatrix* b, int stride_y, int stride_x,float** out);
+int maxPoolingSingleSlice(ThreeDMatrix* X, int pooling_height, int pooling_width, int stride_y, int stride_x,int z, float** out);
 
 
 #endif
