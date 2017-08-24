655582eb96b69735c9faaec9d63bd63aede009df
Bin Tang <13inback@gmail.com>
2017-08-24 23:00:03 +0800

    update

--- C/src/convnet_layers.c
+++ C/src/convnet_layers.c
@@ -6,14 +6,16 @@
 #include "convnet_operations.h"
 #include "convnet_layers.h"
 
-ThreeDMatrix* convLayerForward(ThreeDMatrix* X, ThreeDMatrix** F, int number_of_filters, ThreeDMatrix** b, int f_height, int f_width, int stride_y, int stride_x) {
+ThreeDMatrix* convLayerForward(ThreeDMatrix* X, ThreeDMatrix** F, int number_of_filters, ThreeDMatrix** b, int f_height, int f_width, int stride_y, int stride_x, int padding_y, int padding_x) {
     ThreeDMatrix* V = matrixMalloc(sizeof(ThreeDMatrix));
-    int V_height = calcOutputSize(X->height,0,f_height,stride_y);
-    int V_width = calcOutputSize(X->width,0,f_width,stride_x);
+    int V_height = calcOutputSize(X->height,padding_y,f_height,stride_y);
+    int V_width = calcOutputSize(X->width,padding_x,f_width,stride_x);
     init3DMatrix(V, number_of_filters, V_height, V_width);
+    ThreeDMatrix* X_padded = zeroPadding(X, padding_y, padding_x, false);
     for(int i=0;i<number_of_filters;i++) {
-        convSingleFilter(X,F[i],b[i],stride_y,stride_x,V->d[i]);
+        convSingleFilter(X_padded,F[i],b[i],stride_y,stride_x,V->d[i]);
     }
+    destroy3DMatrix(X_padded);
     return V;
 }
 
