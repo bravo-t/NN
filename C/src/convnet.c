ecfb348fab82a6aa2c2b38ca0100679dda359fa0
Bin Tang <13inback@gmail.com>
2017-08-30 08:47:15 +0800

    update

--- C/src/convnet.c
+++ C/src/convnet.c
@@ -9,5 +9,21 @@
 
 int trainConvnet(ConvnetParameters* network_params) {
     ThreeDMatrix** training_data = network_params->X;
-    
+    /* INPUT -> [[CONV -> RELU]*N -> POOL?]*M -> [FC -> RELU]*K -> FC */
+    int N = network_params->N;
+    int M = network_params->M;
+    int* filter_stride_x = network_params->filter_stride_x;
+    int* filter_stride_y = network_params->filter_stride_y;
+    int* filter_width = network_params->filter_width;
+    int* filter_height = network_params->filter_height;
+    bool* enable_maxpooling = network_params->enable_maxpooling;
+    int* pooling_stride_x = network_params->pooling_stride_x;
+    int* pooling_stride_y = network_params->pooling_stride_y;
+    int* pooling_width = network_params->pooling_width;
+    int* pooling_height = network_params->pooling_height;
+    for(int i=0;i<M;i++) {
+        for(int j=0;j<N;j++) {
+            
+        }
+    }
 }
