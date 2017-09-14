ea7c21e6541630919afc52374e7f47dac8fbc28f
Bin Tang <13inback@gmail.com>
2017-09-14 09:27:46 +0800

    update

---
+++ C/helper_scripts/convnet_unit_test.py
@@ -0,0 +1,96 @@
+#!/bin/env python3
+
+import numpy as np
+
+def xrange(N):
+    return range(N)
+
+def conv_forward_naive(x, w, b, conv_param):
+    stride, pad = conv_param['stride'], conv_param['pad']
+    N, C, H, W = x.shape
+    F, C, HH, WW = w.shape
+    x_padded = np.pad(x, ((0, 0), (0, 0), (pad, pad), (pad, pad)), mode='constant') #补零
+    H_new = 1 + (H + 2 * pad - HH) / stride
+    W_new = 1 + (W + 2 * pad - WW) / stride
+    s = stride
+    out = np.zeros((N, F, H_new, W_new))
+
+    for i in xrange(N):       # ith image    
+        for f in xrange(F):   # fth filter        
+            for j in xrange(H_new):            
+                for k in xrange(W_new):                
+                    out[i, f, j, k] = np.sum(x_padded[i, :, j*s:HH+j*s, k*s:WW+k*s] * w[f]) + b[f]#对应位相乘
+
+    cache = (x, w, b, conv_param)
+
+    return out, cache
+
+
+def conv_backward_naive(dout, cache):
+    x, w, b, conv_param = cache
+    pad = conv_param['pad']
+    stride = conv_param['stride']
+    F, C, HH, WW = w.shape
+    N, C, H, W = x.shape
+    H_new = 1 + (H + 2 * pad - HH) / stride
+    W_new = 1 + (W + 2 * pad - WW) / stride
+
+    dx = np.zeros_like(x)
+    dw = np.zeros_like(w)
+    db = np.zeros_like(b)
+
+    s = stride
+    x_padded = np.pad(x, ((0, 0), (0, 0), (pad, pad), (pad, pad)), 'constant')
+    dx_padded = np.pad(dx, ((0, 0), (0, 0), (pad, pad), (pad, pad)), 'constant')
+
+    for i in xrange(N):       # ith image    
+        for f in xrange(F):   # fth filter        
+            for j in xrange(H_new):            
+                for k in xrange(W_new):                
+                    window = x_padded[i, :, j*s:HH+j*s, k*s:WW+k*s]
+                    db[f] += dout[i, f, j, k]                
+                    dw[f] += window * dout[i, f, j, k]                
+                    dx_padded[i, :, j*s:HH+j*s, k*s:WW+k*s] += w[f] * dout[i, f, j, k]#上面的式子，关键就在于+号
+
+    # Unpad
+    dx = dx_padded[:, :, pad:pad+H, pad:pad+W]
+
+    return dx, dw, db
+
+def max_pool_forward_naive(x, pool_param):
+    HH, WW = pool_param['pool_height'], pool_param['pool_width']
+    s = pool_param['stride']
+    N, C, H, W = x.shape
+    H_new = 1 + (H - HH) / s
+    W_new = 1 + (W - WW) / s
+    out = np.zeros((N, C, H_new, W_new))
+    for i in xrange(N):    
+        for j in xrange(C):        
+            for k in xrange(H_new):            
+                for l in xrange(W_new):                
+                    window = x[i, j, k*s:HH+k*s, l*s:WW+l*s] 
+                    out[i, j, k, l] = np.max(window)
+
+    cache = (x, pool_param)
+
+    return out, cache
+
+
+def max_pool_backward_naive(dout, cache):
+    x, pool_param = cache
+    HH, WW = pool_param['pool_height'], pool_param['pool_width']
+    s = pool_param['stride']
+    N, C, H, W = x.shape
+    H_new = 1 + (H - HH) / s
+    W_new = 1 + (W - WW) / s
+    dx = np.zeros_like(x)
+    for i in xrange(N):    
+        for j in xrange(C):        
+            for k in xrange(H_new):            
+                for l in xrange(W_new):                
+                    window = x[i, j, k*s:HH+k*s, l*s:WW+l*s]                
+                    m = np.max(window)               #获得之前的那个值，这样下面只要windows==m就能得到相应的位置
+                    dx[i, j, k*s:HH+k*s, l*s:WW+l*s] = (window == m) * dout[i, j, k, l]
+
+    return dx
+
