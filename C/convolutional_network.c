7bf28a8c16d80c797d50ff47a55f7772fd3d8af2
Bin Tang <13inback@gmail.com>
2017-11-01 08:54:27 +0800

    update

--- C/convolutional_network.c
+++ C/convolutional_network.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <malloc.h>
 #include <stdbool.h>
-#include <string.h>
+#include <malloc.h>
+#include <math.h>
 #include "src/network_type.h"
 #include "src/matrix_operations.h"
 #include "src/layers.h"
-#include "src/fully_connected_net.h"
 #include "src/misc_utils.h"
+#include "src/fully_connected_net.h"
+#include "src/convnet_operations.h"
+#include "src/convnet_layers.h"
+#include "src/convnet.h"
 
 int main(int argc, char** argv) {
     if (argc == 1) {
         printf("Please specify config file\n");
         exit(0);
     }
-    FCParameters* network_params = readNetworkConfigFile(argv[1]);
+    ConvnetParameters* network_params = readConvnetConfigFile(argv[1]);
     if (strcmp(network_params->mode,"train")) {
-        TwoDMatrix* scores = NULL;
-        test(network_params,socres);
+    	TwoDMatrix* scores = NULL;
+        testConvnet(network_params,scores);
     } else {
-        train(network_params);
+        trainConvnet(network_params);
     }
 }
