4c6ddf4523b0dabca79eac8a6a151f2a1fe0b542
Bin Tang <13inback@gmail.com>
2017-11-14 21:22:23 +0800

    update

--- C/src/matrix_operations_multithread.c
+++ C/src/matrix_operations_multithread.c
@@ -41,6 +41,7 @@ int init2DMatrix_MT(TwoDMatrix* M, int height, int width, int number_of_threads)
             exit(-1);
         }
     } 
+    pthread_attr_destroy(&attr);
     M->initialized = true;
     return 0;
 }
@@ -87,6 +88,7 @@ int init2DMatrixZero_MT(TwoDMatrix* M, int height, int width, int number_of_thre
             exit(-1);
         }
     } 
+    pthread_attr_destroy(&attr);
     M->initialized = true;
     return 0;
 }
@@ -133,6 +135,7 @@ int init2DMatrixOne_MT(TwoDMatrix* M, int height, int width, int number_of_threa
             exit(-1);
         }
     } 
+    pthread_attr_destroy(&attr);
     M->initialized = true;
     return 0;
 }
@@ -185,6 +188,7 @@ int init2DMatrixNormRand_MT(TwoDMatrix* M, int height, int width, float mean, fl
             exit(-1);
         }
     } 
+    pthread_attr_destroy(&attr);
     M->initialized = true;
     return 0;
 }
@@ -206,7 +210,7 @@ int dotProduct_MT(TwoDMatrix* X, TwoDMatrix* M, TwoDMatrix* OUT, int number_of_t
     if (X->width != M->height) {
         return 1;
     }
-    init2DMatrix(OUT,X->height,M->width);
+    init2DMatrix_MT(OUT,X->height,M->width,number_of_threads);
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
@@ -236,6 +240,7 @@ int dotProduct_MT(TwoDMatrix* X, TwoDMatrix* M, TwoDMatrix* OUT, int number_of_t
             exit(-1);
         }
     }        
+    pthread_attr_destroy(&attr);
     return 0;
 }
 
@@ -253,4 +258,83 @@ void* dotProductRow(void* args) {
     }
     free(args);
     pthread_exit(NULL);
+}
+
+int transpose2DMatrix_MT(TwoDMatrix* M,TwoDMatrix* OUT, int number_of_threads) {
+    init2DMatrix_MT(OUT, M->width,M->height,number_of_threads);
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_t* thread = malloc(sizeof(pthread_t)*number_of_threads);
+    int H = M->height / number_of_threads + 1;
+    int t = 0;
+    for(;t<number_of_threads;t++) {
+        struct TwoMatrixOperationsRowArgs* thread_arg = malloc(sizeof(struct TwoMatrixOperationsRowArgs));
+        thread_arg->M = M;
+        thread_arg->OUT = OUT;
+        thread_arg->h_start = t*H;
+        if (thread_arg->h_start >= X->height) break;
+        thread_arg->h_end = (t+1)*H-1;
+        if (thread_arg->h_end >= X->height) thread_arg->h_end = X->height - 1;
+        int create_error = pthread_create(&thread[t],&attr,transpose2DMatrixRow,(void*) thread_arg);
+        if (create_error) {
+            printf("ERROR: Create thread failed.\n");
+            exit(-1);
+        }
+    }
+    void* status;
+    for(int n=0;n<t;n++) {
+        int join_error = pthread_join(thread[n],&status);
+        if (join_error) {
+            printf("ERROR: Join thread failed.\n");
+            exit(-1);
+        }
+    }        
+    pthread_attr_destroy(&attr);
+    return 0;
+}
+
+void* transpose2DMatrixRow(void* args) {
+    struct TwoMatrixOperationsRowArgs* a = (struct TwoMatrixOperationsRowArgs*) args;
+    TwoDMatrix* M = a->M;
+    TwoDMatrix* OUT = a->OUT;
+    for(int i=a->h_start;i<=a->h_end;i++) {
+        for(int j=0;j<M->width;j++) OUT->d[j][i] = M->d[i][j];
+    }
+    free(args);
+    pthread_exit(NULL);
+}
+
+int twoDMatrixOperationMultithreadWrapperMOUT(TwoDMatrix* M, TwoDMatrix* OUT, void* (*func)(void *), int number_of_threads) {
+    init2DMatrix_MT(OUT, M->width,M->height,number_of_threads);
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    pthread_t* thread = malloc(sizeof(pthread_t)*number_of_threads);
+    int H = M->height / number_of_threads + 1;
+    int t = 0;
+    for(;t<number_of_threads;t++) {
+        struct TwoMatrixOperationsRowArgs* thread_arg = malloc(sizeof(struct TwoMatrixOperationsRowArgs));
+        thread_arg->M = M;
+        thread_arg->OUT = OUT;
+        thread_arg->h_start = t*H;
+        if (thread_arg->h_start >= X->height) break;
+        thread_arg->h_end = (t+1)*H-1;
+        if (thread_arg->h_end >= X->height) thread_arg->h_end = X->height - 1;
+        int create_error = pthread_create(&thread[t],&attr,func,(void*) thread_arg);
+        if (create_error) {
+            printf("ERROR: Create thread failed.\n");
+            exit(-1);
+        }
+    }
+    void* status;
+    for(int n=0;n<t;n++) {
+        int join_error = pthread_join(thread[n],&status);
+        if (join_error) {
+            printf("ERROR: Join thread failed.\n");
+            exit(-1);
+        }
+    }        
+    pthread_attr_destroy(&attr);
+    return 0;
 }
