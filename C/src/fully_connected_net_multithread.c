321c687f2aae638b743ff78a5085ca852e311e44
Bin Tang <13inback@gmail.com>
2018-01-03 11:03:39 +0800

    update

--- C/src/fully_connected_net_multithread.c
+++ C/src/fully_connected_net_multithread.c
@@ -581,22 +581,22 @@ void* FCNET_calcLoss_slave(void* args) {
 
 int FCNET_backwardPropagation() {
     for (int i=network_depth-1; i>=0; i--) {
-                if (i != network_depth-1) {
-                    leakyReLUBackward(dHs[i],Hs[i],alpha,dHs[i], number_of_threads);
-                    if (use_batchnorm) {
-                        batchnorm_backward(dHs[i], Hs[i], Hs_normalized[i], gammas[i], betas[i], means[i], vars[i], batchnorm_eps, dHs[i],  dgammas[i], dbetas[i], number_of_threads);
-                    }
-                }
-                //debugPrintMatrix(dHs[i]);
-                if (i != 0) {
-                    affineLayerBackword(dHs[i],Hs[i-1],Ws[i],bs[i],dHs[i-1],dWs[i],dbs[i], number_of_threads);
-                } else {
-                    affineLayerBackword(dHs[i],X,Ws[i],bs[i],dX,dWs[i],dbs[i], number_of_threads);
-                }
-                //debugPrintMatrix(dWs[i]);
-                //debugPrintMatrix(Ws[i]);
-                // Weight changes contributed by L2 regulization
-                L2RegLossBackward(dWs[i],Ws[i],reg_strength,dWs[i], number_of_threads);
-                //debugPrintMatrix(dWs[i]);
+        if (i != network_depth-1) {
+            leakyReLUBackward(dHs[i],Hs[i],alpha,dHs[i], number_of_threads);
+            if (use_batchnorm) {
+                batchnorm_backward(dHs[i], Hs[i], Hs_normalized[i], gammas[i], betas[i], means[i], vars[i], batchnorm_eps, dHs[i],  dgammas[i], dbetas[i], number_of_threads);
             }
+        }
+        //debugPrintMatrix(dHs[i]);
+        if (i != 0) {
+            affineLayerBackword(dHs[i],Hs[i-1],Ws[i],bs[i],dHs[i-1],dWs[i],dbs[i], number_of_threads);
+        } else {
+            affineLayerBackword(dHs[i],X,Ws[i],bs[i],dX,dWs[i],dbs[i], number_of_threads);
+        }
+        //debugPrintMatrix(dWs[i]);
+        //debugPrintMatrix(Ws[i]);
+        // Weight changes contributed by L2 regulization
+        L2RegLossBackward(dWs[i],Ws[i],reg_strength,dWs[i], number_of_threads);
+        //debugPrintMatrix(dWs[i]);
+    }
 }
